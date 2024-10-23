9a0fa3fe5e8645aef6138eebc9ee13eee1797472
anahi hinojosa <ana11hinograje@gmail.com>
2024-10-23 14:04:04 +0200

    creando Readme

--- app/src/main/java/com/ahinojosa/nicestart/Login.java
+++ app/src/main/java/com/ahinojosa/nicestart/Login.java
@@ -1,8 +1,10 @@
 package com.ahinojosa.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this)
+                .load(R.drawable.girl)
+                        .transition(DrawableTransitionOptions.withCrossFade(2000))
+                                .centerCrop()
+                                        .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_700)))
+                                                .into(mGirl);
+
+
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
