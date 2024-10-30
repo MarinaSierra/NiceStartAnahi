e751346d90bfdd477555803575e91ab9e23a518d
anahi hinojosa <ana11hinograje@gmail.com>
2024-10-30 13:55:24 +0100

    Actualizar Readme

--- app/src/main/java/com/ahinojosa/nicestart/Splash.java
+++ app/src/main/java/com/ahinojosa/nicestart/Splash.java
@@ -3,6 +3,10 @@ package com.ahinojosa.nicestart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +15,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Splash extends AppCompatActivity {
-
+    private ImageView img;
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
         openApp();
+
+        ImageView img = findViewById(R.id.logo);
+        TextView text = findViewById(R.id.textView);
+        Animation myanim2 = AnimationUtils.loadAnimation(this,R.anim.transicion);
+        text.startAnimation(myanim2);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.blink);
+        img.startAnimation(myanim);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -36,6 +49,6 @@ public class Splash extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
-        },5000);
+        },3000);
     }
 }
