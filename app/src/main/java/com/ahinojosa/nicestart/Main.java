086cc84b086f9d5d971482c80706707687101c6a
anahi hinojosa <ana11hinograje@gmail.com>
2024-11-18 14:00:38 +0100

    menu en la parte de arriba hecho

--- app/src/main/java/com/ahinojosa/nicestart/Main.java
+++ app/src/main/java/com/ahinojosa/nicestart/Main.java
@@ -1,14 +1,19 @@
 package com.ahinojosa.nicestart;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
@@ -16,10 +21,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
-    private TextView textView;
+    private WebView miVisorWeb;
     private SwipeRefreshLayout swipeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +34,17 @@ public class Main extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.vistaweb);
-        registerForContextMenu(textView);
+      WebView mycontext = (WebView) findViewById(R.id.webView);
+        registerForContextMenu(mycontext);
 
         swipeLayout = findViewById(R.id.myswipe);
         swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+        miVisorWeb = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = miVisorWeb.getSettings();
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        miVisorWeb.loadUrl("https://thispersondoesnotexist.com");
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -46,23 +58,53 @@ public class Main extends AppCompatActivity {
         mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
-//            Toast toast0 = Toast.makeText(Main.this,"Hi there! I don't exit: ",Toast.LENGTH_SHORT);
-//            toast0.show();
-
-            final ConstraintLayout mLayout = findViewById(R.id.myMainConstraint);
-            Snackbar snackbar = Snackbar
-                    .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
-                    .setAction("UNDO", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
-                            snackbar1.show();
-                        }
-                    });
-
-            snackbar.show();
+            Toast toast0 = Toast.makeText(Main.this, "Hi there! I don't exist :)", Toast.LENGTH_LONG);
+            toast0.show();
+            miVisorWeb.reload();
+            swipeLayout.setRefreshing(false);
         }
     };
+
+    private void showAlertDialogButtonClicked(Main mainActivity) {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Achtung!");
+        builder.setMessage("Where do you go?");
+        builder.setIcon(R.drawable.user);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Scrolling", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Intent intent = new Intent(Main.this, Login.class);
+                startActivity(intent);
+                dialog.dismiss();
+
+            }
+        });
+
+        builder.setNegativeButton("Do nothing", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNeutralButton("Other", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // do something like...
+                System.exit(0);
+
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     public boolean onCreateOptionsMenu (Menu menu){
         getMenuInflater().inflate(R.menu.menu_appbar,menu);
         return true;
@@ -82,8 +124,12 @@ public class Main extends AppCompatActivity {
              Toast toast = Toast.makeText(this, "Fixing", Toast.LENGTH_LONG);
              toast.show();
          }
+         if (id == R.id.item5){
+             showAlertDialogButtonClicked(Main.this);
+         }
          return super.onOptionsItemSelected(item);
      }
+
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
 
         getMenuInflater().inflate(R.menu.menu_context, menu);
